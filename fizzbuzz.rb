09427b74617cd97fe8b932567e731385a295fe03
ctyrrell0512 <colleen.a.tyrrell@gmail.com>
2018-06-06 21:46:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
+puts "Pick a number"
+int =gets.chomp
+
 def fizzbuzz(int)
-   
    if int % 3==0 && int % 5==0
     puts "FizzBuzz"
    elsif int % 3==0
@@ -11,4 +13,4 @@ def fizzbuzz(int)
   end
 end
 
-fizzbuzz(4)
+fizzbuzz(int)
